e04bad15fec3e0a7139567aae78a4c1501e499c2
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-10-19 01:30:39 +0900

    feat(app): 사용자 입력과 결과 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { result } from './calculator.js';
 class App {
-  async run() {}
+  async run() {
+    const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    const resultValue = result(input);
+    MissionUtils.Console.print(`결과 : ${resultValue}`);
+  }
 }
 
 export default App;
