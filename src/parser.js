bd6e440ddd70a7b3c0380d64da29babaeb7b34be
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-10-19 23:19:03 +0900

    feat(parser): 구분자로 문자열을 분리해 숫자 배열로 변환하는 기능 추가

--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,19 @@
+import { makeDelimiterRegex } from './utils/regex.js';
+
 export function parseCustomDelimiter(input) {
   const regexp = /^\/\/(.+)(?:\n|\\n)/;
   const match = input.match(regexp);
+
   if (!match) return { customDelimiter: null, body: input };
+
   const customDelimiter = match[1];
   const body = input.slice(match[0].length);
+
   return { customDelimiter, body };
+}
+
+export function parseNumber(body, delims) {
+  const reg = makeDelimiterRegex(delims);
+  const tokens = body.split(reg);
+  return tokens.map((t) => Number(t));
 }
--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,2 +1,2 @@
 export const escapeRegex = (s) => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-
+export const makeDelimiterRegex = (delims) => new RegExp(delims.map(escapeRegex).join('|'), 'g');
