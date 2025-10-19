4d0c776c09dc2229c06328726927987170e9b072
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-10-19 23:18:50 +0900

    test(parser): 구분자로 문자열을 분리해 숫자 배열로 변환하는 테스트 추가

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -1,6 +1,6 @@
-import { parseCustomDelimiter } from '../src/parser.js';
+import { parseCustomDelimiter, parseNumber } from '../src/parser.js';
 
-describe('parser', () => {
+describe('parser - 커스텀 구분자 파싱', () => {
   test.each([
     ['//;\n1;2;3', ';', '1;2;3'],
     ['//***\n1***2***3', '***', '1***2***3'],
@@ -15,5 +15,17 @@ describe('parser', () => {
     const { customDelimiter, body } = parseCustomDelimiter(input);
     expect(customDelimiter).toBe(expectedDelim);
     expect(body).toBe(expectedBody);
+  });
+});
+
+describe('parser - 숫자 파싱', () => {
+  test.each([
+    ['1;2;3', [',', ':', ';'], [1, 2, 3]],
+    ['1,2:3', [',', ':'], [1, 2, 3]],
+    ['3', [',', ':'], [3]],
+    ['', [',', ':'], [0]],
+  ])('"%s"를 구분자 %j로 파싱하면 %j가 된다', (body, delims, expected) => {
+    const numbers = parseNumber(body, delims);
+    expect(numbers).toEqual(expected);
   });
 });
