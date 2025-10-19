0ab082d923407c538c226169bf3079b5085c4784
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-10-19 23:32:08 +0900

    feat(calculator): 숫자 덧셈 기능 및 통합 계산기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import { result } from './calculator.js';
+
 class App {
   async run() {
     const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    const resultValue = result(input);
-    MissionUtils.Console.print(`결과 : ${resultValue}`);
+    MissionUtils.Console.print(`결과 : ${result(input)}`);
   }
 }
 
--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,20 @@
-import { isEmpty} from './validator.js';
+import { isEmpty, onlyDigitsAndDelims } from './validator.js';
+import { parseCustomDelimiter, parseNumber } from './parser.js';
+
+export function add(number) {
+  const sum = number.reduce((acc, cur) => acc + cur, 0);
+  return sum;
+}
 
 export function result(input) {
   if (isEmpty(input)) return 0;
 
- 
+  const { customDelimiter, body } = parseCustomDelimiter(input);
+  const delimiter = [',', ':', ...(customDelimiter ? [customDelimiter] : [])];
+
+  onlyDigitsAndDelims(body, delimiter);
+
+  const number = parseNumber(body, delimiter);
+
+  return add(number);
 }
